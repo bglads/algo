14ce278f42c1695ed2094758a76a230652fad80a
bglads <bglads@users.noreply.github.com>
2018-06-27 13:53:49 +0530

    Update geodist1.py

--- geodist1.py
+++ geodist1.py
@@ -1,3 +1,12 @@
+'''
+The probability that a machine produces a defective product is 1/3 . What is the probability that the 1st defect is found during the 5th inspection?
+
+IP
+1 3
+5
+
+'''
+
 a=list(map(int,input().strip().split()))
 b=int(input().strip())
 a=float(a[0])/a[1]
